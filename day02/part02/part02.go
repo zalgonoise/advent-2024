8ee05c0756f9429f48897182a53a47b4313fe5d0
zalgonoise <mariozalgo@gmail.com>
2024-12-03 05:32:20 +0100

    advent-2024: day02/part02: improved logic for day 2 part 2

--- day02/part02/part02.go
+++ day02/part02/part02.go
@@ -29,247 +29,177 @@ func ParseTable(input string) ([][]int, error) {
 	return set, nil
 }
 
-func Filter(set [][]int, thresh int) []int {
-	indices := make([]int, 0, len(set))
+func Filter2(sets [][]int, thresh int) []int {
+	indices := make([]int, 0, len(sets))
 
-	for i := range set {
-		skip, s := checkOrder(set[i], true)
-
-		switch {
-		case skip:
-			continue
-		case s.order < 0:
-			if filterDecreasing(set[i], thresh, s.canSkip) {
-				indices = append(indices, i)
-			}
-		case s.order > 0:
-			if filterIncreasing(set[i], thresh, s.canSkip) {
-				indices = append(indices, i)
-			}
+	for i := range sets {
+		if isSafe(sets[i], thresh) {
+			indices = append(indices, i)
 		}
 	}
 
 	return indices
 }
 
-type Set struct {
-	canSkip bool
-	order   int
-	items   []int
-}
-
-func checkOrder(set []int, alreadySkipped bool) (skip bool, out Set) {
-	var order int
-
-	if len(set) < 2 {
-		return true, Set{}
+func isSafe(set []int, thresh int) bool {
+	order, isUnsafe := getOrder(set)
+	if isUnsafe {
+		return false
 	}
 
-	for i := 1; i < len(set)-1; i++ {
+	switch order {
+	case 1:
+		return isAnySafe(breakdownSet(set), thresh, isSafeAscending)
+	case -1:
+		return isAnySafe(breakdownSet(set), thresh, isSafeDescending)
+	}
+
+	return false
+}
+
+func isAnySafe(sets [][]int, thresh int, fn func([]int, int) bool) bool {
+	for i := range sets {
+		if fn(sets[i], thresh) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isSafeAscending(set []int, thresh int) bool {
+	for i := 1; i < len(set); i++ {
+		if set[i] < set[i-1] {
+			return false
+		}
+
+		if set[i]-set[i-1] > thresh {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isSafeDescending(set []int, thresh int) bool {
+	for i := 1; i < len(set); i++ {
+		if set[i] > set[i-1] {
+			return false
+		}
+
+		if set[i-1]-set[i] > thresh {
+			return false
+		}
+	}
+
+	return true
+}
+
+func breakdownSet(set []int) [][]int {
+	sets := make([][]int, 0, len(set)+1)
+	sets = append(sets, set)
+
+	for i := range set {
+		n := make([]int, 0, len(set)-1)
+
+		n = append(n, set[:i]...)
+		n = append(n, set[i+1:]...)
+
+		sets = append(sets, n)
+	}
+
+	return sets
+}
+
+//func isSafeAscending(set []int, thresh int) bool {
+//	var skipped bool
+//	var prev = set[0]
+//
+//	for i := 1; i < len(set); i++ {
+//		switch {
+//		case set[i] <= prev:
+//			if skipped {
+//				return false
+//			}
+//
+//			skipped = true
+//		case set[i]-prev > thresh:
+//			if skipped {
+//				return false
+//			}
+//
+//			skipped = true
+//
+//			if prev == set[0] {
+//				prev = set[i]
+//			}
+//		default:
+//			prev = set[i]
+//		}
+//	}
+//
+//	return true
+//}
+
+//func isSafeDescending(set []int, thresh int) bool {
+//	var skipped bool
+//	var prev = set[0]
+//
+//	for i := 1; i < len(set); i++ {
+//		switch {
+//		case set[i] >= prev:
+//			if skipped {
+//				return false
+//			}
+//
+//			skipped = true
+//		case prev-set[i] > thresh:
+//			if skipped {
+//				return false
+//			}
+//
+//			skipped = true
+//
+//			if prev == set[0] {
+//				prev = set[i]
+//			}
+//		default:
+//			prev = set[i]
+//		}
+//	}
+//
+//	return true
+//}
+
+func getOrder(set []int) (int, bool) {
+	var (
+		asc  int
+		desc int
+
+		order = make([]int, len(set)-1)
+	)
+
+	for i := 1; i < len(set); i++ {
 		switch {
 		case set[i] > set[i-1]:
-			if order < 0 {
-				if !alreadySkipped {
-					return true, Set{}
-				}
-
-				alreadySkipped = false
-
-				prev, cur, _ := cropByIndex(set, i)
-
-				prevSkip, prevOut := checkOrder(prev, false)
-				curSkip, curOut := checkOrder(cur, false)
-
-				switch {
-				case !prevSkip && !curSkip:
-					return true, Set{}
-				case !prevSkip:
-					return false, prevOut
-				case !curSkip:
-					return false, curOut
-				default:
-					return true, Set{}
-				}
-			}
-
-			order = 1
+			order[i-1] = 1
+			asc++
 		case set[i] < set[i-1]:
-			if order > 0 {
-				if !alreadySkipped {
-					return true, Set{}
-				}
-
-				alreadySkipped = false
-
-				prev, cur, _ := cropByIndex(set, i)
-
-				prevSkip, prevOut := checkOrder(prev, false)
-				curSkip, curOut := checkOrder(cur, false)
-
-				switch {
-				case !prevSkip && !curSkip:
-					return true, Set{}
-				case !prevSkip:
-					return false, prevOut
-				case !curSkip:
-					return false, curOut
-				default:
-					return true, Set{}
-				}
-			}
-
-			order = -1
-		default:
+			order[i-1] = -1
+			desc++
 		}
 	}
 
-	return false, Set{canSkip: alreadySkipped, order: order, items: set}
-}
-
-func filterIncreasing(set []int, thresh int, canSkip bool) bool {
-	for i := 1; i < len(set); i++ {
-		switch {
-		case set[i] < set[i-1]:
-			if !canSkip {
-				return false
-			}
-
-			if i == 1 {
-				switch {
-				case set[i] < set[i-1]:
-					prev, cur, _ := cropByIndex(set, i)
-
-					if !filterDecreasing(prev, thresh, false) &&
-						!filterDecreasing(cur, thresh, false) {
-						return false
-					}
-
-					continue
-				case set[i] > set[i-1]:
-					prev, cur, _ := cropByIndex(set, i)
-
-					if !filterIncreasing(prev, thresh, false) &&
-						!filterIncreasing(cur, thresh, false) {
-						return false
-					}
-
-					continue
-				default:
-					return false
-				}
-			}
-		case set[i]-set[i-1] == 0:
-			if !canSkip {
-				return false
-			}
-
-			prev, cur, next := cropByIndex(set, i)
-
-			if !filterIncreasing(prev, thresh, false) &&
-				!filterIncreasing(cur, thresh, false) &&
-				!filterIncreasing(next, thresh, false) {
-				return false
-			}
-		case set[i]-set[i-1] > thresh:
-			if !canSkip {
-				return false
-			}
-
-			prev, cur, _ := cropByIndex(set, i)
-
-			if !filterIncreasing(prev, thresh, false) &&
-				!filterIncreasing(cur, thresh, false) {
-				return false
-			}
-		}
+	switch {
+	case asc > 1 && desc > 1:
+		return 0, true
+	case asc > 1:
+		return 1, false
+	case desc > 1:
+		return -1, false
+	default:
+		return 0, true
 	}
-
-	return true
-}
-
-func filterDecreasing(set []int, thresh int, canSkip bool) bool {
-	for i := 1; i < len(set); i++ {
-		switch {
-		// swapped order
-		// [3,2,3] --> ASC:[2,3], DESC[3,2]
-		case set[i-1] < set[i]:
-			if !canSkip {
-				return false
-			}
-
-			if i == 1 {
-				switch {
-				case set[i] < set[i-1]:
-					prev, cur, _ := cropByIndex(set, i)
-
-					if !filterDecreasing(prev, thresh, false) &&
-						!filterDecreasing(cur, thresh, false) {
-						return false
-					}
-
-					continue
-				case set[i] > set[i-1]:
-					prev, cur, _ := cropByIndex(set, i)
-
-					if !filterIncreasing(prev, thresh, false) &&
-						!filterIncreasing(cur, thresh, false) {
-						return false
-					}
-
-					continue
-				default:
-					return false
-				}
-			}
-
-		// values are the same
-		case set[i-1]-set[i] == 0:
-			if !canSkip {
-				return false
-			}
-
-			prev, cur, next := cropByIndex(set, i)
-			if !filterDecreasing(prev, thresh, false) &&
-				!filterDecreasing(cur, thresh, false) &&
-				!filterDecreasing(next, thresh, false) {
-				return false
-			}
-		// over threshold
-		case set[i-1]-set[i] > thresh:
-			if !canSkip {
-				return false
-			}
-
-			prev, cur, _ := cropByIndex(set, i)
-
-			if !filterDecreasing(prev, thresh, false) &&
-				!filterDecreasing(cur, thresh, false) {
-				return false
-			}
-		}
-	}
-
-	return true
-}
-
-func cropByIndex(set []int, index int) (prev, cur, next []int) {
-	prev = make([]int, 0, len(set)-1)
-	prev = append(prev, set[:index-1]...)
-	prev = append(prev, set[index:]...)
-
-	cur = make([]int, 0, len(set)-1)
-	cur = append(cur, set[:index]...)
-	cur = append(cur, set[index+1:]...)
-
-	if index == len(set)-1 {
-		return prev, cur, set[:index]
-	}
-
-	next = make([]int, 0, len(set)-1)
-	next = append(next, set[:index+1]...)
-	next = append(next, set[index+2:]...)
-
-	return prev, cur, next
 }
 
 func parseTableItem(item string) ([]int, error) {
--- day02/part02/part02_test.go
+++ day02/part02/part02_test.go
@@ -31,117 +31,140 @@ func TestPart02(t *testing.T) {
 			name:   "Challenge",
 			input:  day02.Input,
 			thresh: 3,
-			wants:  618,
+			wants:  711,
 		},
 	} {
 		t.Run(testcase.name, func(t *testing.T) {
 			values, err := ParseTable(testcase.input)
 			is.NoError(t, err)
 
-			filtered := Filter(values, testcase.thresh)
+			filtered := Filter2(values, testcase.thresh)
 
 			is.Equal(t, testcase.wants, len(filtered))
 		})
 	}
 }
 
-func TestFilter(t *testing.T) {
+func TestOrder(t *testing.T) {
 	for _, testcase := range []struct {
-		name   string
-		input  []int
-		thresh int
-		wants  bool
+		name  string
+		input []int
+		order int
+		skip  bool
 	}{
 		{
-			name:   "Success/Ascending",
-			input:  []int{1, 2, 3, 4, 5},
-			thresh: 3,
-			wants:  true,
+			name:  "Ascending",
+			input: []int{1, 2, 3, 4, 5},
+			order: 1,
+			skip:  false,
 		},
 		{
-			name:   "Success/Descending",
-			input:  []int{5, 4, 3, 2, 1},
-			thresh: 3,
-			wants:  true,
+			name:  "Descending",
+			input: []int{5, 4, 3, 2, 1},
+			order: -1,
+			skip:  false,
 		},
 		{
-			name:   "Success/Ascending/WithSwap/Index0",
-			input:  []int{3, 2, 3, 4, 5},
-			thresh: 3,
-			wants:  true,
+			name:  "Descending/WithSkip",
+			input: []int{5, 4, 8, 2, 1},
+			order: -1,
+			skip:  false,
 		},
 		{
-			name:   "Success/Ascending/WithSwap/Index1",
-			input:  []int{1, 0, 3, 4, 5},
-			thresh: 3,
-			wants:  true,
+			name:  "Descending/SwapOnStart",
+			input: []int{3, 4, 3, 2, 1},
+			order: -1,
+			skip:  false,
 		},
 	} {
 		t.Run(testcase.name, func(t *testing.T) {
-			n := Filter([][]int{testcase.input}, testcase.thresh)
-			if !testcase.wants {
-				is.Equal(t, 0, len(n))
+			order, isUnsafe := getOrder(testcase.input)
 
-				return
-			}
-
-			is.Equal(t, 1, len(n))
+			is.Equal(t, testcase.skip, isUnsafe)
+			is.Equal(t, testcase.order, order)
 		})
 	}
 }
 
-func TestOrder(t *testing.T) {
+func TestIsSafe_Ascending(t *testing.T) {
 	for _, testcase := range []struct {
-		name     string
-		input    []int
-		order    int
-		skip     bool
-		wantSets int
+		name  string
+		input []int
+		wants bool
 	}{
 		{
-			name:     "Ascending",
-			input:    []int{1, 2, 3, 4, 5},
-			order:    1,
-			skip:     false,
-			wantSets: 1,
+			name:  "OK/NoInterval",
+			input: []int{1, 2, 3, 4, 5},
+			wants: true,
 		},
 		{
-			name:     "Descending",
-			input:    []int{5, 4, 3, 2, 1},
-			order:    -1,
-			skip:     false,
-			wantSets: 1,
+			name:  "OK/MaxInterval",
+			input: []int{1, 4, 7, 10, 13},
+			wants: true,
 		},
 		{
-			name:     "Descending/WithSkip",
-			input:    []int{5, 4, 8, 2, 1},
-			order:    -1,
-			skip:     false,
-			wantSets: 1,
+			name:  "OK/WithGap/OnEnd",
+			input: []int{1, 4, 7, 10, 9},
+			wants: true,
 		},
 		{
-			name:     "Descending/SwapOnStart",
-			input:    []int{3, 4, 3, 2, 1},
-			order:    -1,
-			skip:     false,
-			wantSets: 1,
+			name:  "OK/WithGap/OnMiddle",
+			input: []int{1, 4, 7, 6, 8},
+			wants: true,
 		},
 		{
-			name:     "Descending/SwapOnStart",
-			input:    []int{3, 4, 3, 2, 1},
-			order:    -1,
-			skip:     false,
-			wantSets: 1,
+			name:  "OK/WithGap/OnStart",
+			input: []int{3, 2, 3, 4, 5},
+			wants: true,
+		},
+		{
+			name:  "OK/WithGap/OnEnd/Equal",
+			input: []int{1, 4, 7, 10, 10},
+			wants: true,
+		},
+		{
+			name:  "OK/WithGap/OnMiddle/Equal",
+			input: []int{1, 4, 7, 7, 8},
+			wants: true,
+		},
+		{
+			name:  "OK/WithGap/OnStart/Equal",
+			input: []int{2, 2, 3, 4, 5},
+			wants: true,
+		},
+		{
+			name:  "OK/IntervalOverflow/Once/Start",
+			input: []int{1, 6, 7, 8, 9},
+			wants: true,
+		},
+		{
+			name:  "OK/IntervalOverflow/Once/Middle",
+			input: []int{5, 6, 13, 8, 9},
+			wants: true,
+		},
+		{
+			name:  "OK/IntervalOverflow/Once/End",
+			input: []int{5, 6, 7, 8, 15},
+			wants: true,
+		},
+		{
+			name:  "Fail/IntervalOverflow/Start",
+			input: []int{1, 7, 13, 14, 15},
+			wants: false,
+		},
+		{
+			name:  "Fail/IntervalOverflow/Middle",
+			input: []int{1, 2, 7, 8, 9},
+			wants: false,
+		},
+		{
+			name:  "Fail/IntervalOverflow/End",
+			input: []int{5, 6, 7, 13, 21},
+			wants: false,
 		},
 	} {
 		t.Run(testcase.name, func(t *testing.T) {
-			skip, sets := checkOrder(testcase.input, true)
-
-			if skip {
-				is.Equal(t, 0, len(sets.items))
-
-				return
-			}
+			is.Equal(t, testcase.wants, isSafe(testcase.input, 3))
 		})
 	}
 }
